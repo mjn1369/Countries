3dee2ea1d0453682ab5d6cdf22f5442f5c51baa9
mJafarinejad <mjn1369@gmail.com>
2019-05-06 16:16:46 +0430

    added repository interface for upper layer to implement

---
+++ domain/src/main/java/apps/mjn/domain/repository/CountriesRepository.kt
@@ -0,0 +1,8 @@
+package apps.mjn.domain.repository
+
+import apps.mjn.domain.entity.Country
+import io.reactivex.Single
+
+interface CountriesRepository {
+    fun getCountries(): Single<Country>
+}
