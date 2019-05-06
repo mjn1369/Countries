18d95d02e0a510f00c14e90524ac95515a9cfb3f
Mahmoud Jafarinejad <mjn1369@gmail.com>
2019-05-06 22:42:19 +0430

    added failure sealed class to cover different failure states

---
+++ remote/src/main/java/apps/mjn/remote/failure/Failure.kt
@@ -0,0 +1,7 @@
+package apps.mjn.domain.failure
+
+sealed class Failure(override val message: String) : Throwable(message) {
+    class ConnectionError(message: String) : Failure(message)
+    class HttpError(message: String) : Failure(message)
+    class ServerError(message: String) : Failure(message)
+}
