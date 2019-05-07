27b5f7454b5c7a7d5e9892daa1367a31d6ea6fe8
Mahmoud Jafarinejad <mjn1369@gmail.com>
2019-05-07 21:11:35 +0430

    load and listen to view model data in main activity

--- app/src/main/java/apps/mjn/countries/ui/main/MainActivity.kt
+++ app/src/main/java/apps/mjn/countries/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package apps.mjn.countries.ui.main
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import apps.mjn.countries.R
 import apps.mjn.countries.ui.base.BaseActivity
+import apps.mjn.countries.ui.model.Resource
+import apps.mjn.countries.ui.model.ResourceState
 import apps.mjn.countries.ui.viewmodel.GetCountriesViewModel
+import apps.mjn.domain.entity.Country
 
 class MainActivity : BaseActivity() {
 
@@ -14,9 +18,29 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buildViewModel()
+        observeViewModel()
+        getCountries()
     }
 
     private fun buildViewModel(){
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(GetCountriesViewModel::class.java)
+    }
+
+    private fun observeViewModel(){
+        viewModel.getData().observe(this, Observer(::listenToViewModelData))
+    }
+
+    private fun getCountries(){
+        viewModel.load()
+    }
+
+    private fun listenToViewModelData(resource: Resource<List<Country>>?) {
+        resource?.let {
+            when (resource.resourceState) {
+                ResourceState.LOADING -> {}
+                ResourceState.SUCCESS -> {}
+                ResourceState.ERROR -> {}
+            }
+        }
     }
 }
