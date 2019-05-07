703ecbc6f6fc8937cfaa0b41a570dbf9222782a9
Mahmoud Jafarinejad <mjn1369@gmail.com>
2019-05-07 19:20:40 +0430

    added app dagger module

---
+++ app/src/main/java/apps/mjn/countries/dependency/module/AppModule.kt
@@ -0,0 +1,27 @@
+package apps.mjn.countries.dependency
+
+import android.content.Context
+import apps.mjn.countries.app.CountriesApp
+import apps.mjn.countries.executer.ExecutorThread
+import apps.mjn.countries.executer.UIThread
+import apps.mjn.domain.executer.PostExecutionThread
+import apps.mjn.domain.executer.UseCaseExecutor
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(application: CountriesApp): Context = application.applicationContext
+
+    @Provides
+    @Singleton
+    fun provideUseCaseExecutor(): UseCaseExecutor = ExecutorThread()
+
+    @Provides
+    @Singleton
+    fun providePostExecutionThread(): PostExecutionThread = UIThread()
+}
