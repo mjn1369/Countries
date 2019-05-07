debf886e35d5dd4afc5042c741b6379b7604e3bf
Mahmoud Jafarinejad <mjn1369@gmail.com>
2019-05-07 20:12:20 +0430

    added viewmodel dependency injection to main activity and updated its dagger module to provide repository and data source

--- app/src/main/java/apps/mjn/countries/ui/main/MainActivity.kt
+++ app/src/main/java/apps/mjn/countries/ui/main/MainActivity.kt
@@ -3,8 +3,13 @@ package apps.mjn.countries.ui.main
 import android.os.Bundle
 import apps.mjn.countries.R
 import apps.mjn.countries.ui.base.BaseActivity
+import apps.mjn.countries.ui.viewmodel.GetCountriesViewModel
+import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
+
+    @Inject
+    lateinit var viewModel: GetCountriesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/apps/mjn/countries/ui/main/MainModule.kt
+++ app/src/main/java/apps/mjn/countries/ui/main/MainModule.kt
@@ -2,6 +2,10 @@ package apps.mjn.countries.ui.main
 
 import androidx.lifecycle.ViewModel
 import apps.mjn.countries.ui.viewmodel.GetCountriesViewModel
+import apps.mjn.data.datasource.CountriesRemoteDataSource
+import apps.mjn.data.repository.CountriesRepositoryImpl
+import apps.mjn.domain.repository.CountriesRepository
+import apps.mjn.remote.datasource.CountriesRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -14,4 +18,10 @@ abstract class MainModule {
 
     @Binds
     abstract fun bindCountriesListViewModel(getCountriesViewModel: GetCountriesViewModel): ViewModel
+
+    @Binds
+    abstract fun bindCountriesRepository(countriesRepository: CountriesRepositoryImpl): CountriesRepository
+
+    @Binds
+    abstract fun bindCountriesRemoteDataSource(countriesRemoteDataSource: CountriesRemoteDataSourceImpl): CountriesRemoteDataSource
 }
