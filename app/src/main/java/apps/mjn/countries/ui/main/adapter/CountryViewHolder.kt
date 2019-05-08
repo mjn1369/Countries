beba22be6b619495630499acf3b26da98add526b
mJafarinejad <mjn1369@gmail.com>
2019-05-08 13:36:23 +0430

    refactor country list item layout to fit information better

--- app/src/main/java/apps/mjn/countries/ui/main/adapter/CountryViewHolder.kt
+++ app/src/main/java/apps/mjn/countries/ui/main/adapter/CountryViewHolder.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.item_country.view.*
 class CountryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind(item: Country, onClick: (Country) -> (Unit)) = with(itemView) {
         tvCountryItemName.text = item.name
+        tvCountryItemName.isSelected = true
         tvCountryItemAlpha2Code.text = "(${item.alpha2Code})"
         tvCountryItemRegion.text = item.region
         setOnClickListener {
